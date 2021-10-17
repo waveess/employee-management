7b1324b2b85342602ddc3fb3cca2f09b63caf09b
rawanfatima <rawanfatima@outlook.com>
2021-10-17 15:38:00 -0700

    finished the server js file

--- server.js
+++ server.js
@@ -332,3 +332,240 @@ addEmployee = () => {
         });
 };
 
+//updateEmployee()
+updateEmployee = () => {
+    //get employees from employee table
+    const employeeSql = `SELECT * FROM employee`;
+    connection.promise().query(employeeSql, (err, data) => {
+        if(err) throw error;
+
+        const employees = data.map(({ id, first_name, last_name }) => ({ name: first_name + " " + last_name, value: id }));
+
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'name',
+                message: "Which employee would you like to update?",
+                choices: employees
+            }
+        ])
+        .then(empChoice => {
+            const employee = empChoice.name;
+            const params = [];
+            params.push(employee);
+
+            const roleSql = `SELECT * FROM role`;
+
+            connection.promise().query(roleSql, (err, data) => {
+                if(err) throw err;
+
+                const roles = data.map(({ id, title }) => ({ name: title, value: id }));
+
+                inquirer.prompt([
+                    {
+                        type: 'list',
+                        name: 'role',
+                        message: "What is the employee's new role?",
+                        choices: roles
+                    }
+                ])
+                .then(roleChoice => {
+                    const role = roleChoice.role;
+                    params.push(role);
+
+                    let employee = params[0];
+                    params[0] = role;
+                    params[1] = employee;
+
+                    const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+                    connection.query(sql, params, (err, result) => {
+                        if(err) throw err;
+                        console.log("Employee has been updated");
+                        showEmployees();
+                    });
+                });
+            });
+        });
+    });
+};
+
+//updateManager()
+updateManager = ()=>{
+    //get employees from employee table
+    const employeeSql = `SELECT * FROM employee`;
+    connection.promise().query(employeeSql, (err, data) => {
+        if(err) throw err;
+
+        const employees = data.map(({ id, first_name, last_name }) => ({ name: first_name + " " + last_name, value: id }));
+
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'name',
+                message: "Which employee would you like to update?",
+                choices: employees
+            }
+        ])
+        .then(empChoice => {
+            const employee = emppChoice.name;
+            const params = [];
+            params.push(employee);
+
+            const managerSql = `SELECT * FROM employee`;
+
+            connection.promise().query(managerSql, (err, data) => {
+                if(err) throw err;
+                const managers = data.map(({ id, first_name, last_name}) => ({ name: first_name + " " + last_name, value: id }));
+                
+                inquirer.prompt([
+                    {
+                        type: 'list',
+                        name: 'manager',
+                        message: "Who is the employee's manager?",
+                        choices: managers
+                    }
+                ])
+                .then(managerChoice => {
+                    const manager = managerChoice.manager;
+                    params.push(manager);
+
+                    let employee = params[0];
+                    params[0] = manager;
+                    params[1] = employee;
+
+                    const sql = `UPDATE employee SET manager_id = ? WHERE id = ?`;
+
+                    connection.query(sql, params, (err, result) => {
+                        if(err) throw err;
+                        console.log('Employee has been updated!');
+
+                        showEmployees();
+                    });
+                });
+            });
+        });
+    });
+};
+
+//employeeDepartment()
+employeeDepartment = () => {
+    console.log('Showing employee by department...\n');
+    const sql = `SELECT 
+                 employee.first_name,
+                 employee.last_name,
+                 department.name AS department
+            FROM employee
+            LEFT JOIN role ON employee.role_id = role.id
+            LEFT JOIN department ON role.department_id = department.id`;
+
+        connection.promise().query(sql, (err, rows) => {
+            if(err) throw err;
+            console.table(rows);
+            promptUser();
+    });
+};
+
+// deleteDepartment()
+deleteDepartment = () => {
+    const deptSql = `SELECT * FROM department`;
+    connection.promise().query(deptSql, (err, data) => {
+        if(err) throw err;
+
+        const dept = data.map(({ name, id }) => ({ name: name, value: id }));
+
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'dept',
+                message: 'What department would you like to delete?',
+                choices: dept
+            }
+        ])
+        .then(deptChoice => {
+            const dept = deptChoice.dept;
+            const sql = `DELETE FROM department WHERE id = ?`;
+
+            connection.query(sql, dept, (err, result) => {
+                if(err) throw err;
+                console.log("Department successfully deleted!");
+                showDepartments();
+            });
+        });
+    });
+};
+
+//deleteRole()
+deleteRole = () => {
+    const roleSql = `SELECT * FROM role`;
+
+    connection.promise().query(roleSql, (err, data) => {
+        if(err) throw err;
+
+        const role = data.map(({ title, id }) => ({ name: title, value: id }));
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'role',
+                message: 'What role would you like to delete?',
+                choices: role
+            }
+        ])
+        .then(roleChoice => {
+            const role = roleChoice.role;
+            const sql = `DELETE FROM role WHERE id = ?`;
+
+            connection.query(sql, role, (err, result) => {
+                if(err) throw err;
+                console.log("Role successfully deleted!");
+                showRoles();
+            });
+        });
+    });
+};
+
+//deleteEmployee()
+deleteEmployee = () => {
+    //get employee from employee table
+    const employeeSql = `SELECT * FROM employee`;
+
+    connection.promise().query(employeeSql, (err, data) => {
+        if(err) throw err;
+
+        const employees = data.map(({ id, first_name, last_name })=> ({ name: first_name + " " + last_name, value: id }));
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'name',
+                message: 'Which  employee would you like to delete?',
+                choices: employees
+            }
+        ])
+        .then(empChoice => {
+            const employee = empChoice.name;
+
+            const sql = `DELETE FROM employee WHERE id = ?`;
+
+            connection.query(sql, employee, (err, result) => {
+                if(err) throw err;
+                console.log("Employee successfully deleted!");
+                showEmployees();
+            });
+        });
+    });
+};
+
+//viewBudget()
+viewBudget = () => {
+    console.log('Showing budget by department...\n');
+    const sql = `SELECT department_id AS id,
+                 department.name AS department,
+                 SUM(salary) AS budget
+            FROM role JOIN department ON role.department_id = department.id
+            GROUP BY department_id`;
+
+            connection.promise().query(sql, (err, rows) => {
+                if(err) throw err;
+                console.table(rows);
+                promptUser();
+            });
+};
